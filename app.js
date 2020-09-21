0279c6656703afe4b82e29bc36c861518aac2eee
Nitin Ranganath <nitinranganath@gmail.com>
2020-09-21 17:51:30 +0530

    Initial mongoose setup

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 app.set("view engine", "ejs");
+
+// Connecting to MongoDB
+(async function connectDB() {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+})();
 
 app.get("/", (req, res) => {
   res.render("index", {

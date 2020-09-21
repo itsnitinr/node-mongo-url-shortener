f905d8d06c04eb850838a9f6aad3184a14516bff
Nitin Ranganath <nitinranganath@gmail.com>
2020-09-21 19:20:35 +0530

    Add custom styling

--- app.js
+++ app.js
@@ -5,30 +5,55 @@ const ShortUrl = require("./models/ShortUrl");
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
 // Connecting to MongoDB
 const connectDB = require("./config/db");
 connectDB();
 
+// Home page
 app.get("/", async (req, res) => {
-  const urls = await ShortUrl.find();
-  res.render("index", { shortUrls: urls });
+  try {
+    // Get all shortened URLs
+    const urls = await ShortUrl.find();
+    res.render("index", { shortUrls: urls });
+  } catch (err) {
+    console.error(err);
+    res.send("Server Error");
+  }
 });
 
+// Generate short URL
 app.post("/short", async (req, res) => {
-  const url = new ShortUrl({
-    fullUrl: req.body.fullUrl,
-  });
-  await url.save();
-  res.redirect("/");
+  try {
+    // Create a new short URL
+    const url = new ShortUrl({
+      fullUrl: req.body.fullUrl,
+    });
+    // Save it to database
+    await url.save();
+    // Redirect to hompage
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    res.send("Server Error");
+  }
 });
 
 app.get("/:shortUrl", async (req, res) => {
-  const url = await ShortUrl.findOne({ shortUrl: req.params.shortUrl });
-  if (!url) res.sendStatus(404);
-  url.clicks++;
-  await url.save();
-  res.redirect(url.fullUrl);
+  try {
+    // Get shortened url from database
+    const url = await ShortUrl.findOne({ shortUrl: req.params.shortUrl });
+    // If it doesn't exist, send 404
+    if (!url) return res.sendStatus(404);
+    // If it exists, increment count and redirect to fullUrl
+    url.clicks++;
+    await url.save();
+    res.redirect(url.fullUrl);
+  } catch (err) {
+    console.error(err);
+    res.send("Server Error");
+  }
 });
 
 app.listen(process.env.PORT, () => {

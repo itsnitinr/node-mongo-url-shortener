d3a5788f2ecfd390406d614fb1a174b035ae24c6
Nitin Ranganath <nitinranganath@gmail.com>
2020-09-21 18:04:24 +0530

    Create mongoose schema for generating short urls

---
+++ models/ShortUrl.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const shortid = require("shortid");
+
+const shortUrlSchema = new mongoose.Schema({
+  fullUrl: {
+    type: String,
+    required: true,
+  },
+  shortUrl: {
+    type: String,
+    required: true,
+    default: shortid.generate,
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
+
+module.exports = mongoose.model("ShortUrl", shortUrlSchema);
